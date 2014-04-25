1f17b1fb03cb75f57c1b951fd5bdf55cfd611756
Mirna Milic <mmilic1@etf.unsa.ba>
2014-04-25 10:02:33 +0200

    production_serve_static_assets_true

--- config/routes.rb
+++ config/routes.rb
@@ -56,6 +56,8 @@ end
   get '/img/:name', to: redirect {|params, req| "/assets/#{params[:name]}.#{params[:format]}" }
   get '/images/:name', to: redirect {|params, req| "/assets/#{params[:name]}.#{params[:format]}" }
   get '/fonts/:name', to: redirect {|params, req| "/assets/#{params[:name]}.#{params[:format]}" }
+  get '/assets/:name', to: redirect {|params, req| "/assets/#{params[:name]}.#{params[:format]}" }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
