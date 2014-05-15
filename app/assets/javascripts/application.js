05d13561c8ab700006c7595382f888d34ca46898
Mirna Milic <mmilic1@etf.unsa.ba>
2014-05-15 21:47:29 +0200

    naslovna

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //= require jsapi
-//= require jquery_ujs
 //= require jquery
+//= require jquery_ujs
 //= require bootstrap
 //= require underscore
 //= require angular
