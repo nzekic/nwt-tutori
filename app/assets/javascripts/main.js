0151936ea011025676e93a12abf93337b81d1168
Mirna Milic <mmilic1@etf.unsa.ba>
2014-06-06 12:33:15 +0200

    profil zapocet

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,7 +6,7 @@ OglasnikZaTutore.config( ['$routeProvider',
             .when('/home', {templateUrl: 'templates/home.html'})
             .when('/', {templateUrl: 'templates/home.html'})
             .when('/subject/:id', {templateUrl: 'templates/subject-view.html'})
-            .when('/profile/:id', {templateUrl: 'templates/profile.html'})
+            .when('/my_profile', {templateUrl: 'templates/profile.html'})
             .when('/ads/search/ads_search', {templateUrl: 'templates/ads_search.html'});
 
     }
