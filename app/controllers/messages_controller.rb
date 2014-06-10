71598dae279a84f4afe594fb6529105e2f4059d2
nzekic <naida.696@gmail.com>
2014-06-10 12:20:13 +0200

    fix

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,15 @@
 class MessagesController < ApplicationController
+	respond_to :json
 
 	def show
+    end
+
+   def index
+    respond_with Message.all
   end
 
 def messages
-		
+		respond_with Message.all
 end
     
 
