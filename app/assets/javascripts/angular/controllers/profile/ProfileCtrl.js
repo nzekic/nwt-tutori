4bde78951d08b2b15fafd988edff24901ee1555e
Mirna Milic <mmilic1@etf.unsa.ba>
2014-06-10 11:37:30 +0200

    Rute popravljene

--- app/assets/javascripts/angular/controllers/profile/ProfileCtrl.js
+++ app/assets/javascripts/angular/controllers/profile/ProfileCtrl.js
@@ -2,6 +2,7 @@ OglasnikZaTutore.controller ('ProfileCtrl',  ['$scope', '$http', '$routeParams',
 	function($scope, $http, $routeParams){
         var id = $routeParams.id
         $scope.sameUser=false;
+
 		$http({
             url: 'http://localhost:3000/profiles/' + id + '/user_profile.json',
     		dataType: 'json',
@@ -13,6 +14,8 @@ OglasnikZaTutore.controller ('ProfileCtrl',  ['$scope', '$http', '$routeParams',
 
 		}).success(function(response){
     		$scope.user_profile = response;
+            var currentUser = $resource("http://localhost:3000/users/:id")
+            var user = currentUser.get({userId:2}, function() {
             if ($scope.user_profile.id == $scope.user_profile.current_user.id){
                 $scope.sameUser = true;
             }
