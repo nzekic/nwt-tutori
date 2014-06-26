dc5b0f9cb7c0cd60c6272c2b3f1d8016f5536eff
nzekic <naida.696@gmail.com>
2014-06-26 12:38:46 +0200

    popravljen prikaz poruka + izgled slanja

--- app/assets/javascripts/angular/controllers/messages/MessageViewCtrl.js
+++ app/assets/javascripts/angular/controllers/messages/MessageViewCtrl.js
@@ -1,19 +1,19 @@
 OglasnikZaTutore.controller ('MessageViewCtrl',  ['$scope', 'Message', '$routeParams', '$http',
 	function($scope, Message, $routeParams, $http){
         var id = $routeParams.id
-   $scope.newSubject = "TEST";
-   $scope.newSubject = Message.getOne(id).subject;
+  // $scope.newSubject = "TEST";
+   //$scope.newSubject = Message.getOne(id).subject;
 
-            $scope.createMessage = function () {
-            var attr = {};
-            attr.sender_id = 1;
-            attr.receiver_id = $scope.newReceiver;
-            attr.content = $scope.newContent;
-            attr.subject = $scope.newSubject;
-            var newMessage = Message.create(attr);
-            $scope.messages.push(newMessage);
+//            $scope.createMessage = function () {
+  //          var attr = {};
+    //        attr.sender_id = 1;
+      //      attr.receiver_id = $scope.newReceiver;
+        //    attr.content = $scope.newContent;
+          //  attr.subject = $scope.newSubject;
+            //var newMessage = Message.create(attr);
+   //         $scope.messages.push(newMessage);
     
-        };
+     //   };
 
 		$http({
     		url: 'http://localhost:3000/messages/' + id + '/view_message.json',
